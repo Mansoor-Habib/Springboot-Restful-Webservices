8b4f738e1d672bc241ca09947565b41628c42ed0
micromax <micromax@DESKTOP-VH4K21I>
2024-10-26 11:03:47 +0530

    First Commit

---
+++ src/main/java/Springboot_rest_webservices/restful_webservices/jpa/PostRepository.java
@@ -0,0 +1,9 @@
+package Springboot_rest_webservices.restful_webservices.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Springboot_rest_webservices.restful_webservices.user.Post;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+}
---
+++ src/main/java/Springboot_rest_webservices/restful_webservices/versioning/PersonV1.java
@@ -0,0 +1,20 @@
+package Springboot_rest_webservices.restful_webservices.versioning;
+
+public class PersonV1 {
+	private String name;
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV1 [name=" + name + "]";
+	}
+
+}
